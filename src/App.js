a83775c53ede91b0ce4bf4bbeb1f4c29bbda6eee
henriquewho <henriquegfonseca89@gmail.com>
2022-04-02 20:59:43 -0300

    added scss file and login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'; 
+import './scss/App.scss'; 
 
 function App() {
-  return (
-    <div>Test</div>
-  )
+    const [logged, setLogged] = useState(false); 
+
+    const joinRoom = () => {
+        setLogged(true); 
+    }
+
+    return (
+        <div className='main-app'>
+            {
+                (logged===false) ? 
+                <div className='login-page'>
+                    <div className='login-card'>
+                        <input type='text' placeholder='name...'></input>
+                        <input type='text' placeholder='room name...'></input>
+                        <button onClick={joinRoom}>Create or Join a game</button>
+                    </div>
+                </div>
+                : 
+                'the game'
+            }
+        </div>
+    )
 }
 
 export default App
