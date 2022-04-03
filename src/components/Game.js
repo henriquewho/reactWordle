edd69cba081b46f883d27c2728210af583f191a6
henriquewho <henriquegfonseca89@gmail.com>
2022-04-02 22:29:32 -0300

    started key functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,10 +7,47 @@ export const GameContext = createContext();
 
 function Game() {
     const [board, setBoard] = useState(boardDefault);
+    const [currAttempt, setCurrAttempt] = useState({
+        attempt: 0, letterPos: 0
+    })
+
+    const onSelectLetter = (keyVal) => {
+        if (currAttempt.letterPos > 4) return; 
+        const newBoard = [...board]; 
+        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal; 
+        setBoard(newBoard)
+        setCurrAttempt((prev) => {
+            return {
+                ...prev, letterPos: prev.letterPos+1
+            }
+        })
+    }
+    
+    const onDelete = () => {
+        if (currAttempt.letterPos === 0) return; 
+        const newBoard = [...board]; 
+        newBoard[currAttempt.attempt][currAttempt.letterPos-1] = ""; 
+        setBoard(newBoard)
+        setCurrAttempt((prev) => {
+            return {
+                ...prev, letterPos: prev.letterPos-1
+            }
+        })
+    }
+
+    const onEnter = () => {
+        if (currAttempt.letterPos !== 5) return; 
+        setCurrAttempt((prev) => {
+            return {
+                attempt: prev.attempt+1, letterPos: 0
+            }
+        })
+    }
 
     return (
         <div className='game'>
-            <GameContext.Provider value={{board, setBoard}}>
+            <GameContext.Provider value={{board, setBoard, currAttempt, setCurrAttempt,
+            onSelectLetter, onDelete, onEnter}}>
                 <Board /> 
                 <Keyboard /> 
             </GameContext.Provider>
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GameContext } from './Game'
 
 function Key({keyVal}) {
+    const {onDelete, onEnter, onSelectLetter} = useContext(GameContext); 
+
+    const selectLetter = () => {
+        if (keyVal==='ENTER') {
+            onEnter()
+        } else if (keyVal==='DEL') {
+            onDelete()
+        } else onSelectLetter(keyVal); 
+    }
+
     return (
-        <div className='key' id={(keyVal==='ENTER' || keyVal==='DEL') ? 'big':''}>{keyVal}</div>
+        <div className='key' onClick={selectLetter}
+        id={(keyVal==='ENTER' || keyVal==='DEL') ? 'big':''}>{keyVal}
+        </div>
     )
 }
 
