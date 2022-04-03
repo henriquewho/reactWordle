058ac7561f05815027c727c36f2133a1f4abc2bb
henriquewho <henriquegfonseca89@gmail.com>
2022-04-02 22:48:08 -0300

    keyboard functionality

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useEffect, useCallback, useContext} from 'react'
 import Key from './Key';
+import {GameContext} from './Game'
 
 function Keyboard() {
+    const {onEnter, onDelete, onSelectLetter} = useContext(GameContext); 
+
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+    const handleKeyboard = useCallback(e=>{
+        if (e.key==='Enter') {
+            onEnter(); 
+        } else if (e.key === 'Backspace') {
+            onDelete(); 
+        } else {
+            const allKeys = [...keys1, ...keys2, ...keys3]; 
+            allKeys.forEach (each=>{
+                if (e.key.toUpperCase() === each) onSelectLetter(each); 
+            })
+        }
+    })
+
+    useEffect (()=>{
+        document.addEventListener('keydown', handleKeyboard)
+
+        return ()=>{
+            document.removeEventListener('keydown', handleKeyboard)
+        }
+    }, [handleKeyboard])
+
     return (
-        <div className='keyboard'>
+        <div className='keyboard' onKeyDown={handleKeyboard}>
             <div className='line1'>
                 {keys1.map((each, index) => {
                     return <Key key={index} keyVal={each}/>
